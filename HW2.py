357923d7d5dc90d4aebebd7f83aa33a492796a24
angelobelloo <angelobello200515@gmail.com>
2025-10-01 12:20:31 +0800

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,14 @@
+
+def char_frequency(string):
+    frequency = {}
+    for char in string:
+        if char.isalpha():
+            char = char.lower()
+            frequency[char] = frequency.get(char, 0) + 1
+    return frequency
+
+input_string = input("Enter string: ")
+result = char_frequency(input_string)
+
+for char, count in result.items():
+    print(f"{char}={count}", end=", ")
